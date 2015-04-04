98fea47e9430acab9e1d4471bd235b626cfd75a5
Anthony Zotti <azotti1337@gmail.com>
2015-04-04 15:22:42 -0700

    Extract signUp method into authFactory

--- src/app/login/authFactory.js
+++ src/app/login/authFactory.js
@@ -16,6 +16,29 @@
         }
       });
     };
+
+    factory.signUp = function(ref, scope, state) {
+      ref.createUser({
+        email    : scope.email,
+        password : scope.password
+      }, function(error, authData) {
+        if (error) {
+          console.log('Error creating user:', error);
+        } else {
+          ref.authWithPassword({
+            email    : scope.email,
+            password : scope.password
+          }, function(error, authData) {
+            if (error) {
+              console.log('Login Failed!', error);
+            } else {
+              chrome.runtime.sendMessage({action: 'setToken', token: authData.token});
+              state.go('main');
+            }
+          });
+        }
+      });
+    };
     return factory;
   });
 })();
